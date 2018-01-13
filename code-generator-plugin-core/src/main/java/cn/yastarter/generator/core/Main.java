9f0b61f00b7cbe9f7c1861916539fbe6e5b79bc0
hh23485 <hh23485@126.com>
2018-01-13 17:36:55 +0800

    init projects

---
+++ code-generator-plugin-core/src/main/java/cn/yastarter/generator/core/Main.java
@@ -0,0 +1,7 @@
+package cn.yastarter.generator.core;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ code-generator-plugin-core/src/test/java/cn/yastarter/generator/core/MainTest.java
@@ -0,0 +1,22 @@
+package cn.yastarter.generator.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void mainTest(){
+
+    }
+
+
+}
