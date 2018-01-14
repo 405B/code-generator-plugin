e35dde32480d6c28d574057575e629f38ae7d4eb
Codemupf <mupengfei6688@163.com>
2018-01-14 19:25:16 +0800

    update maven config

--- code-generator-plugin-core/src/main/java/cn/yastarter/generator/core/Main.java
+++ code-generator-plugin-core/src/main/java/cn/yastarter/generator/core/Main.java
@@ -1,7 +1,10 @@
 package cn.yastarter.generator.core;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-
+        log.error("test");
     }
 }
