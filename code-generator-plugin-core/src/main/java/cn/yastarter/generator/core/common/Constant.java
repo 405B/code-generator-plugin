56af29f43f226e4c08d483c16ffcc1bf33c916e0
Codemupf <mupengfei6688@163.com>
2018-01-14 23:13:20 +0800

    Complete the config file load function

---
+++ code-generator-plugin-core/src/main/java/cn/yastarter/generator/core/common/Constant.java
@@ -0,0 +1,20 @@
+package cn.yastarter.generator.core.common;
+
+/**
+ * @author OovEver
+ * 2018/1/14 23:02
+ */
+public class Constant {
+    public static final String NOT = "0";
+    public static final String YES = "1";
+    public enum DbType {
+        /**
+         * mysql database
+         */
+        MYSQL,
+        /**
+         * oracle database
+         */
+        ORACLE
+    }
+}
---
+++ code-generator-plugin-core/src/test/java/cn/yastarter/generator/core/config/GeneratorConfigTest.java
@@ -0,0 +1,17 @@
+package cn.yastarter.generator.core.config;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author OovEver
+ * 2018/1/14 23:10
+ */
+public class GeneratorConfigTest {
+
+    @Test
+    public void init() {
+        GeneratorConfig.init();
+    }
+}
